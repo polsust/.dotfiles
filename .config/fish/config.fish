2492476478efb93f51b211a1ddf516336cbbe0ea
polsust <polsust@gmail.com>
2026-05-16 19:03:32 +0200

    Add fallbacks for fish aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -17,10 +17,35 @@ if status is-interactive
     alias c conf
     alias .2 "cd ../.."
     alias ll "ls -lt"
-    alias ls "lsd --color=auto"
-    alias cp "advcp -g"
-    alias mv "advmv -g"
-    alias cat bat
+    function ls
+        if type -q lsd
+            lsd --color=auto $argv
+        else
+            command ls $argv
+        end
+    end
+    function cp
+        if type -q advcp
+            advcp -g $argv
+        else
+            command cp $argv
+        end
+    end
+    function mv
+        if type -q advmv
+            advmv -g $argv
+        else
+            command mv $argv
+        end
+    end
+    function cat
+        if type -q bat
+            bat $argv
+        else
+            command cat $argv
+        end
+    end
+
     alias psgrep "ps -e | grep -i"
     alias xclass "xprop | grep CLASS"
     alias e exit
@@ -45,7 +70,20 @@ if status is-interactive
     # apps
     alias scim sc-im
     alias rm trash-put
-    alias rmdir trash-put
+    function rm
+        if type -q trash-put
+            trash-put $argv
+        else
+            command rm $argv
+        end
+    end
+    function rmdir
+        if type -q trash-put
+            trash-put $argv
+        else
+            command rmdir $argv
+        end
+    end
     alias r ranger
     alias z zathura
     alias lg lazygit
@@ -61,7 +99,7 @@ if status is-interactive
     set fish_greeting
 end
 
-zoxide init --cmd cd fish | source
+type -q zoxide && zoxide init --cmd cd fish | source
 
 # pnpm
 set -gx PNPM_HOME "/home/$USER/.local/share/pnpm"
