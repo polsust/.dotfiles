2fbc94e6c697350b4c94f2a114af5dda7b7d8846
polsust <polsust@gmail.com>
2026-05-25 13:07:11 +0200

    Fix jsonpath

--- .config/nvim/lua/plugins/lsp/jsonpath.lua
+++ .config/nvim/lua/plugins/lsp/jsonpath.lua
@@ -2,21 +2,23 @@ return {
   enabled = true,
   "phelipetls/jsonpath.nvim",
   ft = { "json", "json5", "jsonc", "jsonnet" },
-  keys = {
-    {
-      "yp",
-      function()
-        local path = require("jsonpath").get():sub(2)
-
-        vim.fn.setreg("+", path)
-
-        print(path .. " Copied!")
-      end,
-    },
-  },
   config = function()
+    require("jsonpath").setup({
+      show_on_winbar = true,
+    })
+
     if vim.fn.exists("+winbar") == 1 then
       vim.opt_local.winbar = "%{%v:lua.require'jsonpath'.get()%}"
     end
+
+    vim.keymap.set("n", "yp", function()
+      local path = require("jsonpath").get():sub(2)
+
+      vim.fn.setreg("+", path)
+
+      print(path .. " Copied!")
+    end, {
+      buffer = true,
+    })
   end,
 }
